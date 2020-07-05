53bc3935f744b999afb8755c520dd3320938398d
MarkMegarry <30323234+MarkMegarry@users.noreply.github.com>
2020-07-05 20:29:41 +0100

    Update and rename JsCircuitElm.java to JSCircuitElm.java

--- ThreeFalstad.js/examples/JSCircuitElm.java
+++ ThreeFalstad.js/examples/JSCircuitElm.java
@@ -4,9 +4,9 @@ import jsinterop.annotations.JsType;	//Required for JsInterop
 import jsinterop.annotations.JsPackage;	//Required for jsInterop
 
 @JsType(isNative = false, namespace = JsPackage.GLOBAL)	//JsInterop setup
-public class JsCircuitElm {
+public class JSCircuitElm {
     CircuitElm elm;	//Element object
-    public JsCircuitElm(int elm) {	//Constructor, assigns elm to element
+    public JSCircuitElm(int elm) {	//Constructor, assigns elm to element
         this.elm = circuitjs1.mysim.getElm(elm); 
     }
     //Get voltage difference
