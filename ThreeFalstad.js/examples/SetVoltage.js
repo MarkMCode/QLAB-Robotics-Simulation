6603370570a84271eed3341b17211c84ce479b88
MarkMegarry <30323234+MarkMegarry@users.noreply.github.com>
2020-07-10 13:32:00 +0100

    Update SetVoltage.js

--- ThreeFalstad.js/examples/SetVoltage.js
+++ ThreeFalstad.js/examples/SetVoltage.js
@@ -1,5 +1,5 @@
 //Sets voltage value if the element in question is a voltage source if JSCircuitElm is included in compilation
 //Can be injected into GWT app by placing it in the "war" folder and calling:
-//ScriptInjector.fromUrl("SetVoltage.js").inject(); from the Java source code
+//ScriptInjector.fromUrl("SetVoltage.js").setWindow(ScriptInjector.TOP_WINDOW).inject(); from the Java source code
 var battery = new JSCircuitElm(5);
 battery.setVoltage(10);
