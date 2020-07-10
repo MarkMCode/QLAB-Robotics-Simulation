63eafc5ba0cd9171c22b64cf359f7ffca90f3f65
MarkMegarry <30323234+MarkMegarry@users.noreply.github.com>
2020-07-10 13:31:14 +0100

    Update SetResistance.js

--- ThreeFalstad.js/examples/SetResistance.js
+++ ThreeFalstad.js/examples/SetResistance.js
@@ -1,5 +1,5 @@
 //Sets the resistance of the resistor in the default Falstad circuit to 20k if JSCircuitElm.Java is included in compilation
 //Can be injected into GWT app by placing it in the "war" folder and calling:
-//ScriptInjector.fromUrl("SetResistance.js").inject(); from the Java source code
+//ScriptInjector.fromUrl("SetResistance.js").setWindow(ScriptInjector.TOP_WINDOW).inject(); from the Java source code
 var resistor = new JSCircuitElm(0);
 resistor.setValue(20000);
