f08d677350e8cf864d06b04ebd06511874f9dd9d
MarkMegarry <30323234+MarkMegarry@users.noreply.github.com>
2020-07-10 12:42:07 +0100

    Update LogInductor.js

--- ThreeFalstad.js/examples/LogInductor.js
+++ ThreeFalstad.js/examples/LogInductor.js
@@ -2,5 +2,5 @@
 //Can be injected into GWT app by placing it in the "war" folder and calling:
 //ScriptInjector.fromUrl("LogInductor.js").inject(); from the Java source code
 
-var inductor = new $wnd.JsCircuitElm(4);
+var inductor = new JSCircuitElm(4);
 console.log(inductor.getVoltageDiff());
