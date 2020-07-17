e4939107a9f255a3f642ec140ed76d737df396f7
MarkMegarry <30323234+MarkMegarry@users.noreply.github.com>
2020-07-17 20:57:21 +0100

    Update LogInductor.js

--- ThreeFalstad.js/examples/LogInductor.js
+++ ThreeFalstad.js/examples/LogInductor.js
@@ -1,3 +1,4 @@
+//Created by Mark Megarry in July 2020 as a debug example for ThreeFalstad.js
 //Prints the voltageDiff value of the inductor in the default Falstad circuit to the console if JSCircuitElm.Java is included in compilation
 //Can be injected into GWT app by placing it in the "war" folder and calling:
 //ScriptInjector.fromUrl("LogInductor.js").setWindow(ScriptInjector.TOP_WINDOW).inject(); from the Java source code
