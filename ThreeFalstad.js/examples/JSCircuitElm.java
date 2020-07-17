0667b3560fccfff273d40b3b4a8a7c0db752be8d
MarkMegarry <30323234+MarkMegarry@users.noreply.github.com>
2020-07-17 20:56:54 +0100

    Update JSCircuitElm.java

--- ThreeFalstad.js/examples/JSCircuitElm.java
+++ ThreeFalstad.js/examples/JSCircuitElm.java
@@ -1,3 +1,4 @@
+//Created by Mark Megarry in July 2020 as a debug example for ThreeFalstad.js
 //This code requires the -generateJsInteropExports compiler argument to be passed
 package com.lushprojects.circuitjs1.client;
 import jsinterop.annotations.JsType;	//Required for JsInterop
