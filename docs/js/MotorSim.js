ba352341c8c4ff12493adca12951d43bc80eca6d
MarkMegarry <30323234+MarkMegarry@users.noreply.github.com>
2020-07-18 10:44:26 +0100

    Update MotorSim.js

--- docs/js/MotorSim.js
+++ docs/js/MotorSim.js
@@ -1,6 +1,6 @@
 //Created by Matthew Reaney, cylinder physics updated by Mark Megarry June-July 2020
             var inductor = new JSCircuitElm(2);
-            while(typeof inductor.getCurrent() != 'number'){
+            while(typeof inductor.getCurrent() === "undefined"){
                         console.log("Can't reach CircuitJS1");           
             }
             let physicsWorld, scene, camera, renderer, rigidBodies = [], tmpTrans = null
